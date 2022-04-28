3887fb57d0bde5e5f6877e2b313b4b933588b00a
truc2203 <tranthanhtruc22031998@gmail.com>
2022-04-28 17:27:58 +0700

    Bai tap Js buoi 14

--- BT4_triangle.js
+++ BT4_triangle.js
@@ -1,4 +1,23 @@
+// SƠ ĐỒ 3 KHỐI
 
+/**
+ * INPUT :nhập 3 cạnh A B C
+ * 
+ * PROCESS : 
+ *  1. Input : tạo các biến var edgeAEl, edgeBEl, edgeCEl
+ *  2. Tạo var result4  lưu kết quả loại tam giác
+ *   
+ *  3. Người dùng nhập giá trị vào 3 biến edgeAEl, edgeBEl, edgeCEl
+ *     Nếu cả 3 biến đều là kiểu number : 
+ *       - A = B = c ==> Tam giác đều
+ *       - A = B hoặc A = C hoặc B = C ==> Tam giác cân
+ *       - C*C = A*A + B*B ==> Tam giác vuông
+ *     Nếu 1 trong 3 biến không phải kiểu number :
+ *      - NaN NaN NaN
+ *  4. Log kết quả ra man hình
+ * 
+ * OUTPUT : biến result3 
+ */
 
 
 
